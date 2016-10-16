4ec5f3876f299380040b8f097ef946eb9d1f5cda
leasange <leasange@qq.com>
2016-10-16 16:36:38 +0800

    添加文件

---
+++ SmartAccess/ConfigMgr/FrmModifyUserPwd.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace SmartAccess.ConfigMgr
+{
+    partial class FrmModifyUserPwd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.tbOldPwd = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.tbNewPwd = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.tbRePwd = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnOk = new DevComponents.DotNetBar.ButtonX();
+            this.btnCancel = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(52, 31);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(62, 23);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "原始密码";
+            // 
+            // tbOldPwd
+            // 
+            // 
+            // 
+            // 
+            this.tbOldPwd.Border.Class = "TextBoxBorder";
+            this.tbOldPwd.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.tbOldPwd.Location = new System.Drawing.Point(120, 31);
+            this.tbOldPwd.Name = "tbOldPwd";
+            this.tbOldPwd.PasswordChar = '*';
+            this.tbOldPwd.Size = new System.Drawing.Size(172, 23);
+            this.tbOldPwd.TabIndex = 1;
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(52, 63);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(62, 23);
+            this.labelX2.TabIndex = 0;
+            this.labelX2.Text = "新密码";
+            // 
+            // tbNewPwd
+            // 
+            // 
+            // 
+            // 
+            this.tbNewPwd.Border.Class = "TextBoxBorder";
+            this.tbNewPwd.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.tbNewPwd.Location = new System.Drawing.Point(120, 63);
+            this.tbNewPwd.Name = "tbNewPwd";
+            this.tbNewPwd.PasswordChar = '*';
+            this.tbNewPwd.Size = new System.Drawing.Size(172, 23);
+            this.tbNewPwd.TabIndex = 1;
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(52, 96);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(62, 23);
+            this.labelX3.TabIndex = 0;
+            this.labelX3.Text = "重新输入";
+            // 
+            // tbRePwd
+            // 
+            // 
+            // 
+            // 
+            this.tbRePwd.Border.Class = "TextBoxBorder";
+            this.tbRePwd.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.tbRePwd.Location = new System.Drawing.Point(120, 96);
+            this.tbRePwd.Name = "tbRePwd";
+            this.tbRePwd.PasswordChar = '*';
+            this.tbRePwd.Size = new System.Drawing.Size(172, 23);
+            this.tbRePwd.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnOk.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnOk.Location = new System.Drawing.Point(88, 158);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(83, 28);
+            this.btnOk.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "确定";
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnCancel.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(182, 158);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(83, 28);
+            this.btnCancel.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "取消";
+            // 
+            // FrmModifyUserPwd
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(349, 223);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.tbRePwd);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.tbNewPwd);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.tbOldPwd);
+            this.Controls.Add(this.labelX1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmModifyUserPwd";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "修改密码";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX tbOldPwd;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.Controls.TextBoxX tbNewPwd;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.DotNetBar.Controls.TextBoxX tbRePwd;
+        private DevComponents.DotNetBar.ButtonX btnOk;
+        private DevComponents.DotNetBar.ButtonX btnCancel;
+    }
+}
---
+++ SmartAccess/ConfigMgr/FrmModifyUserPwd.cs
@@ -0,0 +1,62 @@
+﻿using SmartAccess.Common;
+using SmartAccess.Common.Datas;
+using SmartAccess.Common.WinInfo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SmartAccess.ConfigMgr
+{
+    public partial class FrmModifyUserPwd :DevComponents.DotNetBar.Office2007Form
+    {
+        public FrmModifyUserPwd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (tbOldPwd.Text != UserInfoHelper.OldPwd)
+            {
+                MessageBox.Show("原始密码输入错误！");
+                tbOldPwd.Focus();
+                return;
+            }
+            if (tbNewPwd.Text == "")
+            {
+                MessageBox.Show("新密码不能为空！");
+                tbNewPwd.Focus();
+                return;
+            }
+            if (tbNewPwd.Text != tbRePwd.Text)
+            {
+                MessageBox.Show("两次输入密码不相同！");
+                tbRePwd.Focus();
+                return;
+            }
+            //"USER_NAME='" + tbUserName.Text.Trim() + "' and IS_ENABLE=1 and IS_DELETE=0 and PASS_WORD= substring(sys.fn_sqlvarbasetostr(HashBytes('MD5','" + tbPwd.Text + "')),3,32)"
+            CtrlWaiting waiting = new CtrlWaiting(() =>
+            {
+                try
+                {
+                    Maticsoft.DBUtility.DbHelperSQL.ExecuteSql("update SMT_USER_INFO set PASS_WORD=substring(sys.fn_sqlvarbasetostr(HashBytes('MD5','" + tbNewPwd.Text + "')),3,32) where ID='"+UserInfoHelper.UserID+"'");
+                    SmtLog.InfoFormat("用户", "修改密码,用户名：{0}", UserInfoHelper.UserInfo.USER_NAME);
+                    this.Invoke(new Action(() =>
+                    {
+                        this.Close();
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    WinInfoHelper.ShowInfoWindow(this, "修改用户密码失败：" + ex.Message);
+                }
+            });
+            waiting.Show(this);
+        }
+    }
+}
